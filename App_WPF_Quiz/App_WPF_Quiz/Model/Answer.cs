0bef618867fd1df504aff2308cc0e76353db280c
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-06-25 21:24:00 +0200

    dsa

---
+++ App_WPF_Quiz/App_WPF_Quiz/Model/Answer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_WPF_Quiz.Model
+{
+    class Answer
+    {
+    }
+}
---
+++ App_WPF_Quiz/App_WPF_Quiz/Model/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_WPF_Quiz.Model
+{
+    public class Question
+    {
+        public String question { get; set; }
+        public Answers answer { get; set; }
+        public Question(String question, Answers answer)
+        {
+            this.question = question;
+            this.answer = answer;
+
+
+        }
+    }
+}
